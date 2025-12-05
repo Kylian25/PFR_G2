0e92b86ddb02ec8d4dcf7a8dc93dc2b9dc1c8cc8
Kylian Goudelin <kylian.goudelin@gmail.com>
2025-12-05 18:31:55 +0100

    .

---
+++ src/config/config.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "config.h"
+
+
+CONFIG_ROBOT config = {0}
+
+
+void charger_config(const char * chemin_fichier_config){
+
+    return 0;
+}
+
---
+++ src/config/config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H  // éviter les erreurs lors de la conpilation 
+#define CONFIG_H
+
+typedef struct {
+
+    char langue[15];
+    char type_requete[15];
+    int distance_deplacement;
+    int temps_deplacement;
+    int distance_detection;
+    int angle;
+
+}CONFIG_ROBOT;
+
+extern CONFIG_ROBOT config;  // pour l'utiliser dans les autres .c 
+
+
+
+#endif
