c0b4e1a86a73380ab9023deec12c13172a63eab9
Florian Compaore <157723764+compaoreflorian@users.noreply.github.com>
2026-01-21 09:45:13 +0100

    Add files via upload

--- src/images/module_image.c
+++ src/images/module_image.c
@@ -3,11 +3,8 @@
 #include "module_image.h"
 
 int main() {
-char image_1[]="IMG_300/IMG_5404.txt";
-char image_2[]="IMG_300/IMG_5404.jpeg";
-char image_traité[]="image_traité.jpg";
+char image_1[]="IMG_300/IMG_5401.txt";
+char image_2[]="IMG_300/IMG_5401.jpeg";
 afficher_resultats(image_1,image_2);
-
-
-
+return 0;
 }
